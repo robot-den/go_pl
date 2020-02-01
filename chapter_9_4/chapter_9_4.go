0ca332f38a3c92f8b1db7d3c8aa7fefc1084af3b
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2020-02-01 16:05:41 +0300

    Chapter 9.4

---
+++ chapter_9_4/chapter_9_4.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	var x, y int
+	go func() {
+		x = 1                         // A1
+		fmt.Print("y: ", y, " ") // A2
+	}()
+
+	go func() {
+		y = 1						  // B1
+		fmt.Print("x: ", x, " ") // B2
+	}()
+
+	time.Sleep(200 * time.Millisecond)
+	fmt.Println()
+}
